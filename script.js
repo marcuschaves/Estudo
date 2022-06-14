d5464f7f8c8c5a41d28710a13487d059ad049b57
Antonio Marcus Nogueira Chaves Filho <marcus.chaves@ajaxti.com.br>
2022-06-14 16:31:37 -0300

    integration chart with datagrid

--- script.js
+++ script.js
@@ -1,9 +1,82 @@
 $(() => {
+    $("#chart").dxChart({
+        showBorders: true,
+        palette: 'soft',
+        series: {
+          argumentField: "cdFilial",
+          valueField: "indiceReprovacao",
+          name: "Índice de reprovação",
+          type: "stackedBar",
+        },
+      });
+
+    $("#dataGrid").dxDataGrid({
+        dataSource: data,
+        showBorders: true,
+        rowAlternationEnabled: true,
+        selection: {
+            mode: 'single',
+        },
+        paging: {
+          pageSize: 6,
+        },
+        pager: {
+          showNavigationButtons: true,
+        },
+        columns: [{
+            dataField: "cdFilial",
+          },{
+            dataField: "dsGrupoPC",
+          },{
+            dataField: "Data",
+            dataType: "date",
+          },{
+            dataField: "dsPontoControle",
+          },{
+            dataField: "Aprovados",
+          },{
+            dataField: "Reprovados",
+          },{
+            dataField: "Reprovacao",
+          },
+        ],
+        allowColumnReordering: true,
+        onSelectionChanged(selectedItems) {
+            const { dsPontoControle } = selectedItems.selectedRowsData.pop();
+            let dt = data.filter(f => f.dsPontoControle === dsPontoControle);
+            let dataSource = dt.map(item => ({
+                cdFilial: item.cdFilial,
+                indiceReprovacao: ((dt.filter(s => s.cdFilial == item.cdFilial)
+                                        .reduce((a, b) => a + ((b['Aprovados'] || 0) - (b['Reprovados'] || 0)), 0)))*100
+            }));
+
+            if (dataSource) {
+              $('#chart').dxChart({	dataSource });
+            }
+          },
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+$(() => {
   $("#chart").dxChart({
     dataSource: data,
     showBorders: true,
     series: {
-      argumentField: "cdFilial",
+      argumentField: "dsPontoControle",
       valueField: "Reprovacao",
       name: "Índice de reprovação",
       type: "bar",
@@ -36,40 +109,7 @@ $(() => {
 });
 
 $(function () {
-  $("#dataGrid").dxDataGrid({
-    dataSource: data,
-    showBorders: true,
-    rowAlternationEnabled: true,
-    paging: {
-      pageSize: 6,
-    },
-    pager: {
-      showNavigationButtons: true,
-    },
-    columns: [
-      {
-        dataField: "cdFilial",
-      },
-      {
-        dataField: "dsGrupoPC",
-      },
-      {
-        dataField: "Data",
-        dataType: "date",
-      },
-      {
-        dataField: "dsPontoControle",
-      },
-      {
-        dataField: "Aprovados",
-      },
-      {
-        dataField: "Reprovados",
-      },
-      {
-        dataField: "Reprovacao",
-      },
-    ],
-    allowColumnReordering: true,
-  });
+  
 });
+
+*/
