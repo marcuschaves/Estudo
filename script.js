b0ffd41335777401c644023034a087fd461a1d45
Antonio Marcus Nogueira Chaves Filho <marcus.chaves@ajaxti.com.br>
2022-06-15 16:13:21 -0300

    up

--- script.js
+++ script.js
@@ -1,42 +1,87 @@
 $(() => {
-
   const data = [];
-  const pontoDeControle = ['A', 'B', 'C']
+  const pontoDeControle = ["A", "B", "C"];
 
-  while(data.length < 1000){
+  while (data.length < 1000) {
     let grupoControle = pontoDeControle[Math.floor(Math.random() * 3)];
     let aprovados = Math.floor(Math.random() * 100);
-    let reprovados =  Math.floor(Math.random() * 20);
+    let reprovados = Math.floor(Math.random() * 20);
 
     data.push({
-      cdFilial: (Math.floor(Math.random() * 12)) +1,
+      cdFilial: Math.floor(Math.random() * 12) + 1,
       dsGrupoPC: grupoControle,
       dsPontoControle: grupoControle,
       Data: new Date(),
       aprovados,
       reprovados,
-      reprovacao: (reprovados/(aprovados + reprovados)),
+      reprovacao: reprovados / (aprovados + reprovados),
     });
   }
+  const pieChart = $("#pieChart")
+    .dxPieChart({
+      title: "Reprovação",
+      commonSeriesSettings: {
+        argumentField: "cdFilial",
+      },
+      series: [
+        
+        {
+          valueField: "aprovados",
+          name: "Aprovados"
+        },
+        {
+          valueField: "reprovacao",
+          name: "Reprovacao"
+        },
+      ],
+    })
+    .dxPieChart('instance');
 
+  const chart = $("#chart")
+    .dxChart({
+      title:"Não conformidade",
+      name: "Índice de reprovação",
+      showBorders: true,
+      palette: "soft",
+      commonSeriesSettings: {
+        argumentField: "cdFilial",
+        type: "stackedBar",
+      },
+      series: [
+        {
+          valueField: "aprovados",
+          name: "Aprovados",
+        },
+        {
+          valueField: "reprovados",
+          name: "Reprovados",
+        },
+      ],
+    })
+    .dxChart("instance");
 
-  const chart = $("#chart").dxChart({
-    name: "Índice de reprovação",
-    showBorders: true,
-    palette: "soft",
-    commonSeriesSettings: {
-      argumentField: "cdFilial",
-      type: 'stackedBar'
-    },
-    series: [{ 
-      valueField: "aprovados", 
-      name: 'Aprovados' 
-    }, { 
-      valueField: "reprovados", 
-      name: 'Reprovados' 
-    }]
-  }).dxChart('instance');
-
+    const chartVader = $("#chartVader")
+    .dxChart({
+      title:"Não conformidade",
+      name: "Índice de reprovação",
+      showBorders: true,
+      palette: "soft",
+      commonSeriesSettings: {
+        argumentField: "cdFilial",
+        type: "stackedBar",
+      },
+      series: [
+        {
+          valueField: "aprovados",
+          name: "Aprovados",
+        },
+        {
+          valueField: "reprovados",
+          name: "Reprovados",
+        },
+      ],
+    })
+    .dxChart("instance");
   $("#dataGrid").dxDataGrid({
     dataSource: data,
     showBorders: true,
@@ -50,7 +95,8 @@ $(() => {
     pager: {
       showNavigationButtons: true,
     },
-    columns: [{
+    columns: [
+      {
         dataField: "cdFilial",
       },
       {
@@ -73,39 +119,32 @@ $(() => {
         dataField: "reprovacao",
         dataType: "number",
         customizeText: (options) => {
-            return `${(options.value * 100).toFixed(2)}%`;
-        }
+          return `${(options.value * 100).toFixed(2)}%`;
+        },
       },
     ],
     onSelectionChanged(selectedItems) {
       const { dsPontoControle } = selectedItems.selectedRowsData.pop();
       let dt = data.filter((f) => f.dsPontoControle === dsPontoControle);
-      
+
       let dataSource = dt.map((item) => ({
         cdFilial: item.cdFilial,
         aprovados: item.aprovados,
         reprovados: item.reprovados,
+        reprovacao: item.reprovacao,
       }));
 
       if (dataSource) {
         chart.option({
-          dataSource, 
-          
+          dataSource,
         });
+        pieChart.option({
+          dataSource,
+        });
+        chartVader.option({
+          dataSource,
+        })
       }
     },
   });
 });
-
-
-// $(() => {
-//   $("#pieChart").dxPieChart({
-//     dataSource,
-//     series: {
-//       argumentField: "day",
-//       valueField: "maquina",
-//     },
-//   });
-// });
-
-
